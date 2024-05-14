8f434e2d71d99f6a35a14582bd859d0cc9e83d34
abduhamidovnurmuhammad <166822563+abduhamidovnurmuhammad@users.noreply.github.com>
2024-05-14 20:25:21 +0500

    Update test.py
    Versiya 2

--- test.py
+++ test.py
@@ -7,3 +7,12 @@ cursor.execute('''INSERT INTO phch (id, name, date, value)VALUES (0, 'KUPEN', 'C
 cursor.execute('''INSERT INTO students (id, name, faculty, grade)VALUES (1, 'Islom', 'CF', 4)''')
 cursor.execute('''UPDATE students SET grade = 5 WHERE id = 1''')
 cursor.execute('''SELECT * FROM students''')import sqlite3
+
+
+conn = sqlite3.connect('xxxxxx.db')
+cursor = conn.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS medicine (id INT, name TEXT, date INT, value INT)')
+cursor.execute('''INSERT INTO phch (id, name, date, value)VALUES (0, 'KUPEN', 'CF', 5)''')
+cursor.execute('''INSERT INTO students (id, name, faculty, grade)VALUES (1, 'Islom', 'CF', 4)''')
+cursor.execute('''UPDATE students SET grade = 5 WHERE id = 1''')
+cursor.execute('''SELECT * FROM students''')
